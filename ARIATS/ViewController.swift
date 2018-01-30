6d556b464f97ca3d570cd91ed27c61508a22e0ec
Ravern Koh <ravern.koh.dev@gmail.com>
2018-01-30 15:21:47 +0800

    Forget to commit ViewController

--- ARIATS/ViewController.swift
+++ ARIATS/ViewController.swift
@@ -8,21 +8,40 @@
 
 import UIKit
 
-class StudentViewController: UIViewController {
+class ViewController: UIViewController {
     
     @IBOutlet var status: UILabel!
     @IBOutlet var name: UITextField!
     
+    var isTeacher = false
     var node: MeshNetworkNode!
-    var node2: MeshNetworkNode!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        node = MeshNetworkNode(name: "Node1")
-        node2 = MeshNetworkNode(name: "Node2")
+        if name == nil {
+            isTeacher = true
+        }
     }
     
     @IBAction func takeAttendance(_ sender: UIButton) {
+        var name: String?
+        var ingress: Int
+        
+        if isTeacher {
+            name = MeshNetworkNodeTeacherID
+            ingress = 0
+        } else {
+            name = self.name.text
+            ingress = 1
+        }
+
+        if let name = name {
+            node = MeshNetworkNode(name: name, delegate: self, ingress: ingress, egress: 3)
+            sender.isEnabled = false
+        }
     }
+}
+
+extension ViewController: MeshNetworkNodeDelegate {
 }
